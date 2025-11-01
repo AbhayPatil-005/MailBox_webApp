d4c8f70612168ca02c28a2f71ba0d87a3a68df42
AbhayPatil-005 <abhay.official005@gmail.com>
2025-11-01 19:27:57 +0530

    MailBox-Client Project: added some tests for signUp component

---
+++ src/pages/Auth/signUpPage.test.jsx
@@ -0,0 +1,36 @@
+import {render, screen, fireEvent, waitFor} from "@testing-library/react";
+import SignUpPage from "./SignUpPage";
+import '@testing-library/jest-dom';
+
+beforeEach(()=>{
+    global.fetch = vi.fn();
+});
+afterEach(()=>{
+    vi.restoreAllMocks();
+});
+
+test("renders from inputs and submit button", ()=>{
+    render(<SignUpPage/>);
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/^password$/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/confirm Password/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", {name:/submit/i})).toBeInTheDocument();
+});
+
+
+test("shows EMAIL_EXISTS error from API", async () => {
+  global.fetch.mockResolvedValueOnce({
+    ok: false,
+    json: async () => ({
+      error: { message: "EMAIL_EXISTS" },
+    }),
+  });
+
+  render(<SignUpPage />);
+  fireEvent.change(screen.getByPlaceholderText(/email/i), { target: { value: "existing@gmail.com" } });
+  fireEvent.change(screen.getByPlaceholderText(/^password$/i), { target: { value: "123456" } });
+  fireEvent.change(screen.getByPlaceholderText(/confirm password/i), { target: { value: "123456" } });
+  fireEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+  expect(await screen.findByText(/already registered/i)).toBeInTheDocument();
+});
