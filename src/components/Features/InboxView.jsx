9eab2a49d76028870df079044ae01c8104ef91b8
AbhayPatil-005 <abhay.official005@gmail.com>
2025-11-06 22:37:14 +0530

    MailBox-Client Project: feat: added mail delete functionality in inbox view with toast messages for better UX

--- src/components/Features/InboxView.jsx
+++ src/components/Features/InboxView.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Toast, ToastContainer, ListGroup, Spinner, Badge } from "react-bootstrap";
+import { Toast, ToastContainer, ListGroup, Spinner, Button } from "react-bootstrap";
 import { useNavigate, useOutletContext } from "react-router-dom";
 
 const InboxView = () => {
@@ -9,7 +9,7 @@ const InboxView = () => {
     const safeEmail = email.replace(/\./g, ",");
 
     const navigate = useNavigate();
-    const {setUnreadCount} = useOutletContext();
+    const { setUnreadCount } = useOutletContext();
 
     const [mails, setMails] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -40,15 +40,29 @@ const InboxView = () => {
             setLoading(false)
         }
     }
+
+    const handleDelete = async (mailId) => {
+        try {const res = await fetch(`${BASE_URL}/mails/${safeEmail}/inbox/${mailId}.json`,{
+            method:"DELETE",
+        });
+        setMails((prev)=>prev.filter((mail)=>mail.id!==mailId));
+        setToast({show:true, message:"Mail deleted successfully", variant:"success", textColor:"text-white"});
+        } catch (error) {
+            setToast({show:true, message:"Failed to delete Mail, check internet connection!", variant:"danger", textColor:"text-white"});
+            console.log(error.message)
+        };
+    };
+
     useEffect(() => {
         fetchInbox();
     }, []);
+
     if (loading)
         return (
             <div className="d-flex justify-content-center align-items-center" style={{ height: "80vh" }}>
                 <Spinner animation="border" variant="primary" />
-            </div>)
-    ;
+            </div>);
+            
     return (<>
         <ToastContainer position="top-end" className="mt-3">
             <Toast
@@ -80,9 +94,16 @@ const InboxView = () => {
                                     )}
                                     <strong>{mail.from}</strong> — {mail.subject}
                                 </div>
-                                <small>
+
+                                <small className="ms-auto">
                                     {mail.date ? new Date(mail.date).toLocaleDateString() : ""}
                                 </small>
+                                <Button variant="light" size="sm" className="ms-3 rounded-5" onClick={(e) => {
+                                    e.stopPropagation();
+                                    handleDelete(mail.id);
+                                }}> <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20px" height="30px" viewBox="0 0 30 30">
+                                        <path d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"></path>
+                                    </svg></Button>
                             </ListGroup.Item>
                         ))}
                     </ListGroup>
